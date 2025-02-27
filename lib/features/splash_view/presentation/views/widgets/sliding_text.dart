f1e8c0c99df035b12f5e7d7332cdae4a8e0e52f5
Ehabhanna1 <ehabhanna1599@gmail.com>
2025-02-27 18:49:25 +0200

    add sliding animation to splash view

---
+++ lib/features/splash_view/presentation/views/widgets/sliding_text.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class SlidingText extends StatelessWidget {
+  const SlidingText({
+    super.key,
+    required this.slidingAnimation,
+  });
+
+  final Animation<Offset> slidingAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: slidingAnimation,
+      builder: (BuildContext context, _) { 
+           return  SlideTransition(
+        position: slidingAnimation,
+        child: const Text(
+          'Read Free Books',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+            
+          ),
+        ),
+      );
+    
+       },
+       
+    );
+  }
+}
--- lib/features/splash_view/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash_view/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,36 @@
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/splash_view/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>with SingleTickerProviderStateMixin {
+
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    animationController = AnimationController(vsync: this, duration: const Duration(seconds: 1));
+
+    slidingAnimation =Tween<Offset>(begin: const Offset(0,2), end: Offset.zero).animate(animationController);
+   
+      
+    animationController.forward();
+    super.initState();
+  }
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -11,17 +39,10 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(Assets.logo),
-        const SizedBox(height: 5),
-        const Text(
-          'Read Free Books',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.w600,
-            
-          ),
-        ),
+        const SizedBox(height: 4),
+        SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
   }
 }
+
