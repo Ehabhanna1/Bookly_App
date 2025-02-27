6e1fe20cc889b07878677f61f66e74694c998690
Ehabhanna1 <ehabhanna1599@gmail.com>
2025-02-28 01:31:18 +0200

    best seller part 3

--- lib/features/home_view/presentation/views/widgets/home_view_body.dart
+++ lib/features/home_view/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home_view/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bookly_app/features/home_view/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home_view/presentation/views/widgets/feature_books_list_view.dart';
 import 'package:flutter/material.dart';
@@ -31,52 +32,6 @@ class HomeViewBody extends StatelessWidget {
 }
 
 
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 130,
-      child: Row(
-        children: [
-      
-      AspectRatio(
-        aspectRatio: 2.5/3.7,
-        child: Container(
-         
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(8),
-            image: DecorationImage(
-              image: AssetImage(Assets.testImage),fit: BoxFit.fill),
-          ),
-        ),
-      ),
-      const SizedBox(width: 30,),
-      Column(
-        children: [
-          SizedBox(
-            width: MediaQuery.of(context).size.width*0.5,
-            
-           child: Text(
-                      'Harry Potter and the Goblet of Fire',
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: Styles.textStyle20.copyWith(
-                        fontFamily: kGtSectraFine,
-                      ),
-                    ),),
-
-        ],
-      ),
-      
-      
-        ],
-      ),
-    );
-  }
-}
 
 
 
