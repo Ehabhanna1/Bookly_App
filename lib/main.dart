32d3300e159a1449d5c98d89c026488251759061
Ehabhanna1 <ehabhanna1599@gmail.com>
2025-02-27 16:26:04 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:bookly_app/features/splash_view/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Bookly',
+      theme: ThemeData.dark(),
+      home: const SplashView(),
+    );
+  }
+}
+
