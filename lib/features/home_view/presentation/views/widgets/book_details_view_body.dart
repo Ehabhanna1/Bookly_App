dda36087b99c0561b2ef6e0e08f0afac108cf33b
Ehabhanna1 <ehabhanna1599@gmail.com>
2025-03-01 16:24:14 +0200

    refactore book details body &&create two section

--- lib/features/home_view/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home_view/presentation/views/widgets/book_details_view_body.dart
@@ -1,10 +1,6 @@
-import 'package:bookly_app/core/utils/styles.dart';
-import 'package:bookly_app/core/widgets/custom_button.dart';
-import 'package:bookly_app/features/home_view/presentation/views/widgets/book_rating.dart';
-import 'package:bookly_app/features/home_view/presentation/views/widgets/books_action.dart';
+import 'package:bookly_app/features/home_view/presentation/views/widgets/books_details_section.dart';
 import 'package:bookly_app/features/home_view/presentation/views/widgets/custom_boo_details_app_bar.dart';
-import 'package:bookly_app/features/home_view/presentation/views/widgets/custom_book_image.dart';
-import 'package:bookly_app/features/home_view/presentation/views/widgets/similar_books_list_view.dart';
+import 'package:bookly_app/features/home_view/presentation/views/widgets/similar_books_section.dart';
 import 'package:flutter/material.dart';
 
 
@@ -14,7 +10,7 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
+   
 
     return CustomScrollView(
       slivers: [
@@ -26,31 +22,12 @@ class BookDetailsViewBody extends StatelessWidget {
         children: [
       
           const CustomBookDetailsAppBar(),
-          Padding(
-            padding:  EdgeInsets.symmetric(horizontal: width * 0.2),
-            child: CustomBookImage(),
-          ),
-          const SizedBox(height: 40,),
-          const Text("The Jungle Book",style: Styles.textStyle30,),
-          const SizedBox(height: 5,),
-          
-           Text("Rudyard Kipling",style: Styles.textStyle18.copyWith(fontStyle: FontStyle.italic,),),
-          const SizedBox(height: 15,),
-          BookRating(
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-          const SizedBox(height: 35,),
-         const BooksAction(),
+          const BooksDetailsSection(),
+         
           Expanded(child: const SizedBox(height: 40,)),
+          const SimilarBooksSection(),
 
-         Align(
-           alignment: Alignment.centerLeft,
-           child: Text("You can also like",style: Styles.textStyle18.copyWith(
-            fontWeight: FontWeight.w600,
-           ),),
-         ),
-         const SizedBox(height: 10,),
-         const SimilarBooksListView(),
+         
          const SizedBox(height: 20,),
       
         ],
@@ -63,6 +40,10 @@ class BookDetailsViewBody extends StatelessWidget {
    
   }
 }
+
+
+
+
 
 
 
---
+++ lib/features/home_view/presentation/views/widgets/books_details_section.dart
@@ -0,0 +1,34 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home_view/presentation/views/widgets/book_rating.dart';
+import 'package:bookly_app/features/home_view/presentation/views/widgets/books_action.dart';
+import 'package:bookly_app/features/home_view/presentation/views/widgets/custom_book_image.dart';
+import 'package:flutter/material.dart';
+
+class BooksDetailsSection extends StatelessWidget {
+  const BooksDetailsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+ var width = MediaQuery.of(context).size.width;
+    return Column(
+      children: [
+         Padding(
+            padding:  EdgeInsets.symmetric(horizontal: width * 0.2),
+            child: CustomBookImage(),
+          ),
+          const SizedBox(height: 40,),
+          const Text("The Jungle Book",style: Styles.textStyle30,),
+          const SizedBox(height: 5,),
+          
+           Text("Rudyard Kipling",style: Styles.textStyle18.copyWith(fontStyle: FontStyle.italic,),),
+          const SizedBox(height: 15,),
+          BookRating(
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+          const SizedBox(height: 35,),
+         const BooksAction(),
+
+      ],
+    );
+  }
+}
