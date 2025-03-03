4504ccab2746143aa4f32a50ead0c61aa967085e
Ehabhanna1 <ehabhanna1599@gmail.com>
2025-03-03 18:02:03 +0200

    create api service

--- lib/features/home_view/data/models/book_model/book_model.dart
+++ lib/features/home_view/data/models/book_model/book_model.dart
@@ -1,9 +1,11 @@
+import 'package:bookly_app/features/home_view/domain/entities/book_entity.dart';
+
 import 'access_info.dart';
 import 'sale_info.dart';
 import 'search_info.dart';
 import 'volume_info.dart';
 
-class BookModel {
+class BookModel extends BookEntity{
   String? kind;
   String? id;
   String? etag;
@@ -22,7 +24,14 @@ class BookModel {
     this.saleInfo,
     this.accessInfo,
     this.searchInfo,
-  });
+  }) : super(
+    title: volumeInfo!.title!,
+    
+     authorName: volumeInfo.authors?.first ?? "No Author",
+      image: volumeInfo.imageLinks?.thumbnail ?? '',
+       rating: volumeInfo.averageRating,
+        price: 0.0,
+   bookId: id!);
 
   factory BookModel.fromJson(Map<String, dynamic> json) => BookModel(
     kind: json['kind'] as String?,
--- lib/features/home_view/domain/entities/book_entity.dart
+++ lib/features/home_view/domain/entities/book_entity.dart
@@ -1,9 +1,18 @@
 class BookEntity {
+ 
+  final String bookId;
   final String title;
-  final String authorName;
-  final String image;
-  final num rating;
-  final num price;
+  final String? authorName;
+  final String? image;
+  final num? rating;
+  final num? price;
 
-  BookEntity(this.title, this.authorName, this.image, this.rating, this.price);
+  BookEntity(
+    {
+    required this.title,
+     required this.authorName,
+      required this.image,
+       required this.rating,
+        required this.price,
+         required this.bookId});
 }
