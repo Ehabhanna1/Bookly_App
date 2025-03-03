0683199a90a2f774d5f74428c15a65902737c0af
Ehabhanna1 <ehabhanna1599@gmail.com>
2025-03-03 18:42:35 +0200

    implement remote fetch newest books

--- lib/features/home_view/data/data_sources/home_remote_data_source.dart
+++ lib/features/home_view/data/data_sources/home_remote_data_source.dart
@@ -14,25 +14,30 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDataSource{
   HomeRemoteDataSourceImpl(this.apiService);
   @override
   Future<List<BookEntity>> fetchFeaturedBooks() async{
-   var data = await apiService.get(endPoint: "volumes?q=programming&Filtering=free-ebooks");
+   var data = await apiService.get(endPoint: "volumes?Filtering=free-ebooks&q=programming");
    List<BookEntity> books = getBooksList(data);
     return books;
     
     
   }
 
-  List<BookEntity> getBooksList(Map<String, dynamic> data) {
+ 
+  
+  @override
+  Future<List<BookEntity>> fetchNewestBooks() async{
+    var data = await apiService.get(endPoint: "volumes?Filtering=free-ebooks&Sorting=newest&q=programming");
+   List<BookEntity> books = getBooksList(data);
+    return books;
+  }
+
+
+// method to get books list ===>> ركز وسهل الدنيا ع نفسك
+   List<BookEntity> getBooksList(Map<String, dynamic> data) {
     List<BookEntity> books = [];
     for(var bookMap in data["items"]){
       books.add(BookModel.fromJson(bookMap));
     }
     return books;
-  }
-  
-  @override
-  Future<List<BookEntity>> fetchNewestBooks() {
-    // TODO: implement fetchNewestBooks
-    throw UnimplementedError();
   }
   
 }
