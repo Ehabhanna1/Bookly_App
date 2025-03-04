186e1d9f5a84b6f829853d811d2a220d44e65f4f
Ehabhanna1 <ehabhanna1599@gmail.com>
2025-03-04 15:56:53 +0200

    generate book entity adapter

--- lib/features/home_view/domain/entities/book_entity.dart
+++ lib/features/home_view/domain/entities/book_entity.dart
@@ -1,10 +1,18 @@
+import 'package:hive/hive.dart';
+part 'book_entity.g.dart';
+@HiveType(typeId: 0)
 class BookEntity {
- 
+@HiveField(0)
   final String bookId;
+@HiveField(1)
   final String title;
+@HiveField(2)
   final String? authorName;
+@HiveField(3)
   final String? image;
+@HiveField(4)
   final num? rating;
+@HiveField(5)
   final num? price;
 
   BookEntity(
@@ -16,3 +24,8 @@ class BookEntity {
         required this.price,
          required this.bookId});
 }
+
+
+
+
+/// flutter packages pub run build_runner build
